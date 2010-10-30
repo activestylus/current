6c37b342dd7f7aacec2cbf1283e86586f2155f72
Steven Garcia <stevendgarcia@gmail.com>
2010-10-31 01:20:45 +0200

    More rubyish names for boolean methods

--- lib/current.rb
+++ lib/current.rb
@@ -2,26 +2,27 @@ module Current
 
   def controller_is(*attrs)
     attrs.collect{|attr| attr.to_s}.include?(controller_name)
+    alias_method :controller_is?, :controller_is
   end
 
   def action_is(*attrs)
     attrs.map{|attr| attr.to_s}.include?(action_name)
+    alias_method :action_is?, :action_is
   end
 
   def partial_is(param)
     param == params[:partial]
+    alias_method :partial_is?, :partial_is
   end
 
   def controller_action_is(c,a)
     controller_is(c) && action_is(a)
-  end
-
-  def controller_action_is?(c,a)
-    controller_action_is(c,a)
+    alias_method :controller_action_is?, :controller_action_is
   end
 
   def active_if(condition)
     condition ? "active" : "inactive"
+    alias_method :active_if?, :active_if
   end                         
 
   def nav_link_to(text,path,condition, options={})
